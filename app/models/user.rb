28a90b97142868218e777f149761d06f81199cc5
Jacob Klimenko <droidedjake@gmail.com>
2021-02-17 14:15:13 -0500

    Add validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 
 class User < ActiveRecord::Base
-    has_secure_password #password(=), authenticate, password_confirmation
     has_many :deeds, class_name: "Deed", foreign_key: "owner_id"
     has_many :properties, through: :deeds
     has_many :buildings, through: :properties
+
+    has_secure_password #password(=), authenticate, password_confirmation
+    validates :password, presence: :true, length: {minimum: 6, too_short: "A password must be at least %{count} characters long"}, confirmation: true
+    validates :username, presence: :true, uniqueness: true, exclusion: {in: ["admin", "superadmin", "user"]}
+    validates :email, presence: :true, uniqueness: :true, format: {with: /\A(?<username>[^@\s+])@((?<domain_name>[-a-z0-9+])\.(?<domain>[a-z]{2,}))\z/i}
+    validates :name, presence: true
 end
 
