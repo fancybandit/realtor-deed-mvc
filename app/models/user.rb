dfb63d2b188a673e9010119d2882d444b632bbed
Jacob Klimenko <droidedjake@gmail.com>
2021-02-23 22:48:39 -0500

    Include & Extend Slug module in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+require_relative '../../lib/slug'
 
 class User < ActiveRecord::Base
     has_many :deeds, class_name: "Deed", foreign_key: "owner_id"
@@ -10,5 +11,8 @@ class User < ActiveRecord::Base
     validates :username, presence: :true, uniqueness: true, exclusion: {in: ["admin", "superadmin", "user"]}
     validates :email, presence: :true, uniqueness: :true, format: {with: /\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}
     validates :name, presence: true
+
+    include Slug
+    extend Slug
 end
 
