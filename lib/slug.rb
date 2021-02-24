986d4efb9bf922dd9da71ea156868c30fe53c689
Jacob Klimenko <droidedjake@gmail.com>
2021-02-23 22:21:01 -0500

    Create slug.rb with module containing slug methods

---
+++ lib/slug.rb
@@ -0,0 +1,15 @@
+
+module Slug
+
+    def slug
+        self.username.gsub(/[^0-9A-Za-z ]/, '').downcase.split(' ').join('-')
+    end
+
+    def find_by_slug(slug)
+        self.all.detect do |obj|
+            obj.slug == slug
+        end
+    end
+
+end
+
