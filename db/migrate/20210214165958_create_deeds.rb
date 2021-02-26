e805f7c7185ba3b7608c490c2ec3d3d6b087e328
Jacob Klimenko <droidedjake@gmail.com>
2021-02-26 17:08:05 -0500

    Delete deed upon property deletion

--- db/migrate/20210214165958_create_deeds.rb
+++ db/migrate/20210214165958_create_deeds.rb
@@ -2,7 +2,7 @@ class CreateDeeds < ActiveRecord::Migration[5.2]
   def change
     create_table :deeds do |t|
       t.references :owner, foreign_key: {to_table: :users, on_delete: :cascade}
-      t.references :property
+      t.references :property, foreign_key: {on_delete: :cascade}
 
       t.timestamps null: false
     end
