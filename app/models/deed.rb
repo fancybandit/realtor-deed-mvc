45dda6ac45463df537ffbeed416513caefa2b590
Jacob Klimenko <droidedjake@gmail.com>
2021-03-02 21:43:22 -0500

    Fix referential integrity between User & Property

--- app/models/deed.rb
+++ app/models/deed.rb
@@ -1,6 +1,6 @@
 
 class Deed < ActiveRecord::Base
     belongs_to :owner, class_name: "User"
-    belongs_to :property
+    belongs_to :property, dependent: :destroy
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 require_relative '../../lib/slug'
 
 class User < ActiveRecord::Base
-    has_many :deeds, class_name: "Deed", foreign_key: "owner_id"
-    has_many :properties, through: :deeds
-    has_many :buildings, through: :properties
+    has_many :deeds, class_name: "Deed", foreign_key: "owner_id", dependent: :destroy
+    has_many :properties, through: :deeds, dependent: :destroy
+    has_many :buildings, through: :properties, dependent: :destroy
 
     has_secure_password #password(=), authenticate, password_confirmation
     validates :password, presence: :true, length: {minimum: 6, too_short: "A password must be at least %{count} characters long"}, confirmation: true, on: :update
