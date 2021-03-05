57529a5893615b5b33c35e15f87428156dada395
Jacob Klimenko <droidedjake@gmail.com>
2021-03-04 20:28:47 -0500

    Validate current user in User modify routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,8 @@ class UsersController < ApplicationController
   # GET: /users/5/edit
   get "/users/:slug/edit" do
     redirect_if_not_logged_in
-    @user = current_user
+    @user = User.find_by_slug(params[:slug])
+    is_current_user?(@user)
     erb :"/users/edit.html"
   end
   
@@ -48,28 +49,35 @@ class UsersController < ApplicationController
   # PATCH: /users/5
   patch "/users/:slug" do
     user = User.find_by_slug(params[:slug])
-    if current_user == user
-      if user.update(params[:user])
-        flash[:success] = "Successfully updated your account"
-        redirect "/users/#{user.slug}"
-      else
-        flash[:error] = user.errors.full_messages.first
-        redirect "/users/#{user.slug}/edit"
-      end
+    is_current_user?(user)
+    if user.update(params[:user])
+      flash[:success] = "Successfully updated your account"
+      redirect "/users/#{user.slug}"
+    else
+      flash[:error] = user.errors.full_messages.first
+      redirect "/users/#{user.slug}/edit"
     end
   end
 
   # DELETE: /users/5/delete
   delete "/users/:slug" do
     user = User.find_by_slug(params[:slug])
-    if current_user == user
-      if user.destroy
-        flash[:success] = "Successfully deleted account"
-        redirect "/login"
-      else
-        flash[:error] = user.errors.full_messages.first
-        redirect "/users/#{user.slug}"
-      end
+    is_current_user?(user)
+    if user.destroy
+      flash[:success] = "Successfully deleted account"
+      redirect "/login"
+    else
+      flash[:error] = user.errors.full_messages.first
+      redirect "/users/#{user.slug}"
+    end
+  end
+
+  private
+
+  def is_current_user?(user)
+    if current_user != user
+      flash[:error] = "You don't have permission to view this page!"
+      redirect "/users"
     end
   end
 end
